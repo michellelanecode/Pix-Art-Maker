ca1cffceaa061206d9da98426ea11b48cf23c92c
Monique Lane <michellecolemanlane@gmail.com>
2022-04-02 12:10:59 -0400

    Update designs.js

--- designs.js
+++ designs.js
@@ -1,6 +1,16 @@
 // Select color input
 // Select size input
 
+const color = document.getElementById('colorPicker');
+const form = document.getElementById('sizePicker');
+const width = document.getElementById('inputWidth');
+const height = document.getElementById('inputHeight');
+
+form.addEventListener('submit', function (event){
+  event.preventDefault();
+  console.log(height.value, width.value);
+})
+
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
