3a414216c6846e99c62865437f4c94ce0c59c39f
Monique Lane <michellecolemanlane@gmail.com>
2022-04-09 15:13:07 -0400

    update
    Added background image, background music, set up starting grid of 20 px

--- designs.js
+++ designs.js
@@ -1,35 +1,38 @@
 // Select color input
 // Select size input
-const color = document.getElementById('colorPicker').value;
+var color = document.getElementById('colorPicker').value;
 const form = document.getElementById('sizePicker');
 const canvas = document.getElementById('pixelCanvas')
 
-form.addEventListener('submit', function (event){
-  event.preventDefault();
-  const width = document.getElementById('inputWidth').value;
-  const height = document.getElementById('inputHeight').value;
-  makeGrid(width, height)
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const width = document.getElementById('inputWidth').value;
+    const height = document.getElementById('inputHeight').value;
+    makeGrid(width, height)
 })
 
-function changeColor(){
-  this.style.background = color;
+function changeColor() {
+    color = document.getElementById('colorPicker').value;
+    this.style.background = color;
 }
+
+document.getElementById('colorPicker').addEventListener("change", changeColor)
+
 // When size is submitted by the user, call makeGrid()
 function makeGrid(width, height) {
-// take the width and the height and use it to make columns to the width of width and to the height of height.
-var w = 0;
-  var h = 0;
-  for (var h = 0; h < height; h++){
-   var row = canvas.insertRow(0)
-    for (var w = 0; w < width; w++){
-      row.insertCell(0)
+    // take the width and the height and use it to make columns to the width of width and to the height of height.
+    var w = 0;
+    var h = 0;
+    for (var h = 0; h < height; h++) {
+        var row = canvas.insertRow(0)
+        for (var w = 0; w < width; w++) {
+            row.insertCell(0)
+        }
     }
-  }
-  const cells = document.querySelectorAll('td');
-  cells.forEach((cell)=> {
-    cell.addEventListener('click', changeColor)
-  })
+    const cells = document.querySelectorAll('td');
+    cells.forEach((cell) => {
+        cell.addEventListener('click', changeColor)
+    })
+    changeColor();
 }
-
-
-
+makeGrid(20, 20)
